66374a8c990bb51351c27aced536f83c46a70505
Stepan <Smirnov-stepan2004@mail.ru>
2023-04-08 21:22:03 +0300

    Add file

--- Practice_work/Practice_work12/Windoor.py
+++ Practice_work/Practice_work12/Windoor.py
@@ -1,16 +1,19 @@
 class WinDoor:
     def __init__(self, x, y):
+        """Module for area window or door calculation"""
         self.square = x * y
 
 
 class Room:
     def __init__(self, width, length, height):
+        """Constructor accepts length, height, width and list of area windows and doors"""
         self.width = width
         self.length = length
         self.height = height
         self.wd = []
 
     def add_wd(self):  # Function for add a class "WinDoor"
+        """Module for add areas of windows and doors"""
         user = int(input('Enter how many windows and doors: '))
         for i in range(1, user + 1):
             print(f'{i} objects')
@@ -20,20 +23,24 @@ class Room:
 
     @staticmethod
     def rolls():
+        """Module for area roll calculation"""
         roll_width = int(input('Enter the width for roll: '))
         roll_length = int(input('Enter the length for roll: '))
         return roll_length * roll_width
 
     def full_square(self):
+        """Module for full area calculation"""
         return 2 * self.height * (self.length * self.length)
 
     def pasted_square(self):
+        """Module for pasted area calculation."""
         new_square = self.full_square()
         for i in self.wd:
             new_square -= i.square
         return new_square, new_square/self.rolls()
 
     def change_size(self):
+        """Module for change size length, width or height"""
         print('1 - width\n2 - length\n3 - height')
         user = int(input('Enter the parameter: '))
         if user in (1, 2, 3):
--- Practice_work/Practice_work12/program.py
+++ Practice_work/Practice_work12/program.py
@@ -1,14 +1,16 @@
 from Windoor import *
+"""Function for working with user interface"""
 
 
 def create_room():
+    """Module for creating of room"""
     l = int(input('Enter the length: '))
     w = int(input('Enter the width: '))
     h = int(input('Enter the height: '))
-    i = Room(w, l, h)
-    i.add_wd()
-    f = i.full_square()
-    p = i.pasted_square()
+    i = Room(w, l, h)  # Create a class "Room"
+    i.add_wd()  # Addendum area of windows and doors.
+    f = i.full_square()  # Full area calculation.
+    p = i.pasted_square()  # Pasted area calculation.
     print(f'Full square: {f}')
     print(f'Pasted square: {p[0]} and need {p[1]} rolls')
     user = int(input('Continue?\n1 - Yes\n2 - No\nAnswer: '))
