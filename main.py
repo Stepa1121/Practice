c63401803d87aeaf5cfb5cf609a36766184c3018
Stepa <Smirnov-stepan2004@mail.ru>
2023-05-14 19:19:23 +0300

    Add file

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-print("Stepa")
+print("Stepa1q")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
